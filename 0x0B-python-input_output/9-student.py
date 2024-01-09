dd736ae3aec7c440ce6730f9e709898834e4ee95
oregetonii <proffzion30@gmail.com>
2024-01-09 14:27:11 +0000

    student dict att1

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+class Student:
+    """
+    Public Attributes:
+        first_name
+        last_name
+        age
+
+    Public Methods:
+        to_json: retrieves its dictionary representation
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes student with name & age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Returns dictionary description with simple data struct
+        (list, dictionary, dictionary, string)
+        for JSON serialization of obj
+        """
+        return self.__dict__
