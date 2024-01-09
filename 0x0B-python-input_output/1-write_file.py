8e03e94b4284adbca8ff8b46b7742c66293b4a29
oregetonii <proffzion30@gmail.com>
2024-01-09 11:25:38 +0000

    write string to file attempt1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module 1-write_file
+
+Contains a function which writes string to file
+"""
+
+
+def write_file(filename="", text=""):
+    """Write string to file"""
+    with open(filename, 'w', encoding="utf-8") as f:
+        f.write(text)
