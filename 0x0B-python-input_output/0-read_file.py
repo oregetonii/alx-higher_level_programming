a6d63d878b375d6e003ff9089f29a43e1a5edc5f
oregetonii <proffzion30@gmail.com>
2024-01-09 11:14:30 +0000

    second att

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+Module 0-read_file
+
+Contains a function that reads and prints contents of file.
+"""
+
+
 def read_file(filename=""):
+    """Read and print text in a file"""
     with open(filename, 'r', encoding="utf-8") as f:
         for line in f:
             print(line, end='')
