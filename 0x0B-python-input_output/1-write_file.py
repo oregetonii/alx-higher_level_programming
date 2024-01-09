66a160607b95c877072a013179b1842c0eaebc4e
oregetonii <proffzion30@gmail.com>
2024-01-09 11:32:05 +0000

    write string to file attempt3

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,11 +2,11 @@
 """
 Module 1-write_file
 
-Contains a function which writes string to file
+Contains a function which writes string to file, returns num chars written
 """
 
 
 def write_file(filename="", text=""):
-    """Write string to file"""
+    """Write string to file and return num of chars written"""
     with open(filename, 'w', encoding="utf-8") as f:
         return(f.write(text))
