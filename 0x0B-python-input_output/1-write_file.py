891c9399130af3d257e995a72d590f0a961d2722
oregetonii <proffzion30@gmail.com>
2024-01-09 11:55:46 +0000

    correction of docstrings

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,6 +7,6 @@ Contains a function which writes to text file and returns num chars written
 
 
 def write_file(filename="", text=""):
-    """Writes to text file and return num of chars written"""
+    """writes to text file and return num of chars written"""
     with open(filename, 'w', encoding="utf-8") as f:
         return(f.write(text))
